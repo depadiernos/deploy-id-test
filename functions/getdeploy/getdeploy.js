0af31766b4bf13739868d5772817bea2b57abf80
Dennis Padiernos <depadiernos@gmail.com>
2019-12-04 03:55:49 -0600

    refactor axios call into main handler function

--- functions/getdeploy/getdeploy.js
+++ functions/getdeploy/getdeploy.js
@@ -6,36 +6,30 @@ function extractNetlifySiteFromContext(context) {
   return decoded;
 }
 
-function getDeploy(deployId) {
-  const { NF_SECRET } = process.env;
-  const options = {
-    headers: { Authorization: `Bearer ${NF_SECRET}` },
-    baseURL: "https://api.netlify.com/api/v1/deploys/"
-  };
-  const get = () => {
-    axios
-      .get(`${deployId}`, options)
-      .then(res => {
-        console.log(res.data);
-        return res.data;
-      })
-      .catch(err => console.log(err));
-  };
-
-  get()
-}
-
 exports.handler = function(event, context, callback) {
   const { deployId } = JSON.parse(event.body);
   console.log(deployId);
 
   const parsedContext = extractNetlifySiteFromContext(context);
   console.log(parsedContext);
-  const resData = getDeploy(deployId);
-  console.log(resData)
 
-  callback(null, {
-    statusCode: 200,
-    body: "test"
-  });
+  const getDeploy = async deployId => {
+    const { NF_SECRET } = process.env;
+    const options = {
+      headers: { Authorization: `Bearer ${NF_SECRET}` },
+      baseURL: "https://api.netlify.com/api/v1/deploys/"
+    };
+    axios
+      .get(`${deployId}`, options)
+      .then(res => {
+        console.log(res.data);
+        callback(null, {
+          statusCode: 200,
+          body: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  getDeploy(deployId);
 };
