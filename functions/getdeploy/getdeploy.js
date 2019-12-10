f126d35d0518857a04d792eb3993c9776dfb71f1
Dennis Padiernos <depadiernos@gmail.com>
2019-12-10 06:45:29 -0600

    more fixing

--- functions/getdeploy/getdeploy.js
+++ functions/getdeploy/getdeploy.js
@@ -14,10 +14,13 @@ exports.handler = function(event, context, callback) {
   console.log(parsedContext);
 
   const getDeploy = async deployId => {
+    const siteId = "78ff8456-a276-41ba-9925-55480f55ca4a";
     const { NF_SECRET } = process.env;
     const options = {
       headers: { Authorization: `Bearer ${NF_SECRET}` },
-      baseURL: "https://api.netlify.com/api/v1/deploys/"
+      baseURL: deployId
+        ? "https://api.netlify.com/api/v1/deploys/"
+        : `https://api.netlify.com/api/v1/sites/${siteId}/deploys`
     };
     axios
       .get(`${deployId}`, options)
