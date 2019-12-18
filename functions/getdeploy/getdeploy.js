f43b7aec651d334eae151f837cb48a2f55ef9fc8
Dennis Padiernos <depadiernos@gmail.com>
2019-12-18 09:05:12 -0600

    Update getdeploy.js

--- functions/getdeploy/getdeploy.js
+++ functions/getdeploy/getdeploy.js
@@ -7,9 +7,9 @@ function extractNetlifySiteFromContext(context) {
 }
 
 exports.handler = function(event, context, callback) {
-
+console.log(event)
         callback(null, {
           statusCode: 200,
-          body: JSON.stringify(event.body)
+          body: "Success"
         });
 };
