6ec20725b1e22918cc2bec797824f734a7c9846c
Dennis Padiernos <depadiernos@gmail.com>
2019-12-18 09:01:26 -0600

    gut function and just return event.body

--- functions/getdeploy/getdeploy.js
+++ functions/getdeploy/getdeploy.js
@@ -7,31 +7,9 @@ function extractNetlifySiteFromContext(context) {
 }
 
 exports.handler = function(event, context, callback) {
-  const { deployId } = JSON.parse(event.body);
-  console.log(deployId);
 
-  const parsedContext = extractNetlifySiteFromContext(context);
-  console.log(parsedContext);
-
-  const getDeploy = async deployId => {
-    const siteId = "78ff8456-a276-41ba-9925-55480f55ca4a";
-    const { NF_SECRET } = process.env;
-    const options = {
-      headers: { Authorization: `Bearer ${NF_SECRET}` },
-      baseURL: deployId
-        ? "https://api.netlify.com/api/v1/deploys/"
-        : `https://api.netlify.com/api/v1/sites/${siteId}/deploys`
-    };
-    axios
-      .get(`${deployId}`, options)
-      .then(res => {
         callback(null, {
           statusCode: 200,
-          body: JSON.stringify(res.data)
+          body: JSON.stringify(event.body)
         });
-      })
-      .catch(err => console.log(err));
-  };
-
-  getDeploy(deployId);
 };
